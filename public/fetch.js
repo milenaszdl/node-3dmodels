ab00eb7c238c6e30ab74add2097875b777be7078
milenaszdl <milicca070803@mail.ru>
2023-06-12 19:51:10 +0300

    добавлен просмотр инфы о модели

--- public/fetch.js
+++ public/fetch.js
@@ -1,5 +1,7 @@
 const apibutton = document.getElementById("getapi");
 const allmodelsbutton = document.getElementById("showmodels");
+const refreshmodels = document.getElementById("refresh");
+const table = document.getElementById("tablemodels");
 
 async function postAPIKey() {
     const username = document.getElementById('username').value;
@@ -23,7 +25,6 @@ async function postAPIKey() {
 }
 
 function getAllModels() {
-    const table = document.getElementById("tablemodels");
     fetch('/router/allmodels')
         .then((response) => {
             console.log(response);
@@ -44,12 +45,74 @@ function getAllModels() {
                     const tr = document.createElement("tr");
                     const td1 = document.createElement("td");
                     const td2 = document.createElement("td");
+                    const td3 = document.createElement("td");
+                    //инфа о модели
                     td1.innerText = "Name: " + element.modelname;
                     td2.innerText = "ID: " + element._id;
+                    //кнопки
+                    const showbutton = document.createElement("button");
+                    showbutton.innerText = "Show";
+                    showbutton.dataset.id = element._id;
+                    showbutton.classList.add("showbutt");
+                    const delbutton = document.createElement("button");
+                    delbutton.innerHTML = "Delete";
+                    delbutton.dataset.id = element._id;
+                    delbutton.classList.add("delbutt");
+                    td3.appendChild(showbutton);
+                    td3.appendChild(delbutton);
                     tr.appendChild(td1);
                     tr.appendChild(td2);
+                    tr.appendChild(td3);
                     table.appendChild(tr);
                 });
+            }
+        })
+        .catch((err) => {
+            console.log("err:", err);
+            alert("Something wrong: couldn't get model's info (");
+        });
+}
+
+function OneModelInfo(id){
+    const modelheader = document.getElementById("onemodel");
+    modelheader.innerText = "Information about model";
+
+    const modeltext = document.getElementById("modelinfo");
+
+    modelheader.style.display = "flex";
+    modeltext.style.display = "flex";
+
+    //отправляем запрос и вставляем текст о модели
+    fetch(`/router/model/${id}`)
+        .then((response)=>{
+            console.log(response);
+            return response.json();
+        })
+        .then((data)=>{
+            if (!data){
+                modeltext.innerText = "There is no information about this model";
+                return;
+            }
+            if(data){
+                //кнопка чтоб спрятать содерживое раздела
+                const divbutt = document.getElementById("forhidebutton");
+                let hidebutton = document.createElement("button");
+                hidebutton.innerText = "Hide";
+                divbutt.appendChild(hidebutton);
+                hidebutton.addEventListener("click", (event) => {
+                    hidebutton.style.display = "none";
+                    modeltext.style.display = "none";
+                    modelheader.style.display = "none";
+                })
+                //текст о модели
+                modeltext.innerText = "ID : " + data._id + "\n" + 
+                                    "Username : " + data.username + "\n" + 
+                                    "Model's name : " + data.modelname + "\n" +
+                                    "Type of model : " + data.modeltype + "\n" +
+                                    "Description : " + data.description + "\n" +
+                                    "Comments : " + data.comments + "\n" + 
+                                    "Creation date : " + data.creationdate + "\n" +
+                                    "Last change : " + data.lastchange;
             }
         })
         .catch((err) => {
@@ -62,7 +125,19 @@ allmodelsbutton.addEventListener("click", (event) => {
     getAllModels();
 })
 
+refreshmodels.addEventListener("click", (event) => {
+    getAllModels();
+})
+
 apibutton.addEventListener("click", (event) => {
     event.preventDefault();
     postAPIKey();
 })
+
+
+table.addEventListener("click", (event) => {
+    const showmodel = event.target.closest(".showbutt");
+    if (showmodel){
+        OneModelInfo(showmodel.dataset.id);
+    }
+});
